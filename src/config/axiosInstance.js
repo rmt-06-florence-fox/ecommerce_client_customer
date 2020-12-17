1730df13ed878bf72abbdf246060aa6a5286937f
sangsangga <wandaputrasangga@gmail.com>
2020-12-17 09:49:40 +0700

    fix axios baseUrl

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://ecommerce-cms-awesome.herokuapp.com'
 })
 
 export default instance
