b067b82a482a180d5a38ae99456dbbb2e1165875
erick <erick.gunawan70@gmail.com>
2020-12-17 11:35:09 +0700

    changed axios baseUrl

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://ecommerce-server12.herokuapp.com'
 })
 
 export default instance
