35b4e34cb0e4b8c67873fba5554739fd55a2d6ee
efrizal-m <efrizalsy0424@gmail.com>
2020-12-17 13:02:35 +0700

    after first deploy

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,12 +1,26 @@
 <template>
-<div>
+<div v-if="!currentUser">
+    <div class="home">
+    <div class="jumbotron">
+      <h1 class="display-4">Welcome to Mr Bee Ecommerce!</h1>
+      <p class="lead">You are logged in as customer.</p>
+      <hr class="my-4">
+      <p>Let's go get your sweet.</p>
+    </div>
+  </div>
     <router-view />
 </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 
 export default {
-  name: 'Main'
+  name: 'Main',
+  computed: {
+    ...mapState([
+      'currentUser'
+    ])
+  }
 }
 </script>
