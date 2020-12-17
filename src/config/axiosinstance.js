02f32aab656fbe597d42e29e37e1f45716dfc48d
Rudy <hactiv8rud@gmail.com>
2020-12-17 13:03:07 +0700

    commit

--- src/config/axiosinstance.js
+++ src/config/axiosinstance.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  // baseURL: 'https://e-commerce-cms-rud.herokuapp.com'
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://e-commerce-cms-rud.herokuapp.com'
 })
 
 export default instance
