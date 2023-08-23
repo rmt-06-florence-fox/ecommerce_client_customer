7a5d7eff4f2706372ce0146d3dfc28312288aa38
Jalu Wibowo Aji <jaluwibowoaji@SekolahMus-MacBook-Pro-2.local>
2023-08-23 22:51:19 +0700

    new endpoint

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://jarooda-db-ecommerce.herokuapp.com/'
-  // baseURL: 'http://localhost:3000'
+  baseURL: 'https://ecommerceserver-production-dfa5.up.railway.app'
 })
 
 export default instance
